28c14b7f1ee8e3cd6816d3318a5c19d089c37eba
Moritz Zimmer <moritzzimmer@users.noreply.github.com>
2025-10-01 13:58:33 +0200

    some documentation changes

--- variables.tf
+++ variables.tf
@@ -1,11 +1,22 @@
+# ---------------------------------------------------------------------------------------------------------------------
+# REQUIRED PARAMETERS
+# You must provide a value for each of these parameters.
+# ---------------------------------------------------------------------------------------------------------------------
+
 variable "github_repository" {
   description = "Name of the GitHub repository that will run the action, including repository owner (e.g. moritzzimmer/terraform-aws-lambda)"
   type        = string
+
   validation {
     condition     = length(split("/", var.github_repository)) == 2
     error_message = "Repository must be of format $owner/$repository_name"
   }
 }
+
+# ---------------------------------------------------------------------------------------------------------------------
+# OPTIONAL PARAMETERS
+# These parameters have reasonable defaults.
+# ---------------------------------------------------------------------------------------------------------------------
 
 variable "github_refs" {
   description = "Name of the refs (e.g. branches) on which the action will run."
@@ -21,13 +32,13 @@ variable "role_name" {
 }
 
 variable "s3_prefixes" {
-  description = "(Optional) S3 prefixes (bucket name + key) that the role can write/read to/from. E.g. ci-eu-central-1/foo. If not set, no S3 access will be granted."
+  description = "S3 prefixes (bucket name + key) that the role can write/read to/from. E.g. ci-eu-central-1/foo. If not set, no S3 access will be granted."
   type        = list(string)
   default     = []
 }
 
 variable "ecr_repositories" {
-  description = "(Optional) ECR repository names that the role can push to. If not set, no ECR access will be granted."
+  description = "ECR repository names that the role can push to. If not set, no ECR access will be granted."
   type        = list(string)
   default     = []
 }
